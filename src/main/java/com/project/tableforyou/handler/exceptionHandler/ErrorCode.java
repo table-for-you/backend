e099a5825193baedf320a68c14c7cfdde89a9f15
DaeYoung Park <pdy000726@naver.com>
2024-03-13 01:16:47 +0900

    feat - 에러 코드 추가

--- src/main/java/com/project/tableforyou/handler/exceptionHandler/ErrorCode.java
+++ src/main/java/com/project/tableforyou/handler/exceptionHandler/ErrorCode.java
@@ -17,8 +17,10 @@ public enum ErrorCode {
     USER_NOT_FOUND(404, "존재하지 않는 회원입니다."),
     RESTAURANT_NOT_FOUND(404, "존재하지 않는 가게입니다."),
     RESERVATION_NOT_FOUND(404, "해당하는 예약번호가 없습니다."),
-    MENU_NOT_FOUND(404, "존재하지 않는 메뉴입니다.");
+    MENU_NOT_FOUND(404, "존재하지 않는 메뉴입니다."),
 
+    // 서버 에러
+    INTERNAL_SERVER_ERROR(500, "서버 에러입니다. 서버 팀에 연락주세요.");
 
     private final int status;
     private final String message;
--- src/main/java/com/project/tableforyou/handler/exceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/project/tableforyou/handler/exceptionHandler/GlobalExceptionHandler.java
@@ -6,6 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import static com.project.tableforyou.handler.exceptionHandler.ErrorCode.INTERNAL_SERVER_ERROR;
+
 @Slf4j
 @RestControllerAdvice("com.project.tableforyou.controller")     // 해당 패기지만 동작하도록 설정.
 public class GlobalExceptionHandler {
@@ -18,5 +20,10 @@ public class GlobalExceptionHandler {
         return new ResponseEntity(error, HttpStatus.valueOf(error.getStatus()));
     }
 
+    @ExceptionHandler
+    protected ResponseEntity customServerException(Exception ex) {
+        ErrorDto error = new ErrorDto(INTERNAL_SERVER_ERROR.getStatus(), INTERNAL_SERVER_ERROR.getMessage());
+        return new ResponseEntity(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
 }
