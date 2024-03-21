67f708d77d8953b4e266ffed752b1e73362e7a64
DaeYoung Park <pdy000726@naver.com>
2024-03-21 17:07:32 +0900

    refactor - API변경으로 인한 리팩터링

--- src/main/java/com/project/tableforyou/service/MenuService.java
+++ src/main/java/com/project/tableforyou/service/MenuService.java
@@ -3,8 +3,8 @@ package com.project.tableforyou.service;
 import com.project.tableforyou.domain.dto.MenuDto;
 import com.project.tableforyou.domain.entity.Menu;
 import com.project.tableforyou.domain.entity.Restaurant;
-import com.project.tableforyou.handler.exceptionHandler.ErrorCode;
 import com.project.tableforyou.handler.exceptionHandler.CustomException;
+import com.project.tableforyou.handler.exceptionHandler.ErrorCode;
 import com.project.tableforyou.repository.MenuRepository;
 import com.project.tableforyou.repository.RestaurantRepository;
 import lombok.RequiredArgsConstructor;
@@ -24,10 +24,10 @@ public class MenuService {
 
     /* 메뉴 추가 */
     @Transactional
-    public Long save(Long restaurant_id, MenuDto.Request dto) {
+    public Long save(String restaurantName, MenuDto.Request dto) {
 
         log.info("Creating menu");
-        Restaurant restaurant = restaurantRepository.findById(restaurant_id).orElseThrow(() ->
+        Restaurant restaurant = restaurantRepository.findByName(restaurantName).orElseThrow(() ->
                 new CustomException(ErrorCode.RESTAURANT_NOT_FOUND));
 
         dto.setRestaurant(restaurant);
@@ -40,28 +40,28 @@ public class MenuService {
 
     /* 메뉴 리스트 페이징 */
     @Transactional(readOnly = true)
-    public Page<MenuDto.Response> menuPageList(Long restaurant_id, Pageable pageable) {
+    public Page<MenuDto.Response> menuPageList(String restaurant, Pageable pageable) {
 
-        log.info("Finding all menus with restuarant ID: {}", restaurant_id);
-        Page<Menu> menus = menuRepository.findByRestaurantId(restaurant_id, pageable);
+        log.info("Finding all menus with restaurant: {}", restaurant);
+        Page<Menu> menus = menuRepository.findByRestaurantName(restaurant, pageable);
         return menus.map(MenuDto.Response::new);
     }
 
     /* 메뉴 검색 */
     @Transactional(readOnly = true)
-    public Page<MenuDto.Response> menuPageSearchList(Long restaurant_id, String searchKeyword, Pageable pageable) {
+    public Page<MenuDto.Response> menuPageSearchList(String restaurant, String searchKeyword, Pageable pageable) {
 
-        log.info("Find all menus with Restaurant ID: {} and keyword: {}", restaurant_id, searchKeyword);
-        Page<Menu> menus = menuRepository.findByRestaurantIdAndNameContaining(restaurant_id, searchKeyword, pageable);
+        log.info("Find all menus with Restaurant: {} and keyword: {}", restaurant, searchKeyword);
+        Page<Menu> menus = menuRepository.findByRestaurantNameAndNameContaining(restaurant, searchKeyword, pageable);
         return menus.map(MenuDto.Response::new);
     }
 
     /* 메뉴 업데이트 */
     @Transactional
-    public void update(Long id, MenuDto.Request dto) {
+    public void update(String restaurant, Long id, MenuDto.Request dto) {
 
         log.info("Updating menu with ID: {}", id);
-        Menu menu = menuRepository.findById(id).orElseThrow(() ->
+        Menu menu = menuRepository.findByRestaurantNameAndId(restaurant, id).orElseThrow(() ->
                 new CustomException(ErrorCode.MENU_NOT_FOUND));
         menu.update(dto.getName(), dto.getPrice());
         log.info("Menu updated successfully");
@@ -69,10 +69,10 @@ public class MenuService {
 
     /* 메뉴 삭제 */
     @Transactional
-    public void delete(Long id) {
+    public void delete(String restaurant, Long id) {
 
         log.info("Deleting menu with ID: {}", id);
-        Menu menu = menuRepository.findById(id).orElseThrow(() ->
+        Menu menu = menuRepository.findByRestaurantNameAndId(restaurant, id).orElseThrow(() ->
                 new CustomException(ErrorCode.MENU_NOT_FOUND));
         menuRepository.delete(menu);
         log.info("Menu deleted successfully");
