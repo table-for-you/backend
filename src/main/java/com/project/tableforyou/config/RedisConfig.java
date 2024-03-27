446e5d9a674e9bcad9a15229a4693530e1c32b22
DaeYoung Park <pdy000726@naver.com>
2024-03-28 01:51:53 +0900

    feat - redis 비밀번호 추가

--- src/main/java/com/project/tableforyou/config/RedisConfig.java
+++ src/main/java/com/project/tableforyou/config/RedisConfig.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.util.StringUtils;
 
 @Configuration
 public class RedisConfig {
@@ -18,9 +20,17 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int port;
 
+    @Value("${spring.data.redis.password}")
+    private String pass;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(host, port);
+
+        if (StringUtils.hasText(pass)) {
+            redisStandaloneConfiguration.setPassword(pass);
+        }
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
 
     @Bean
