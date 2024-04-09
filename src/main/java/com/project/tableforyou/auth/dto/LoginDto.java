ce99417f2be01ce69bf91fcf599c1e1f7b178348
DaeYoung Park <pdy000726@naver.com>
2024-04-09 16:46:51 +0900

    feat - 필드 조건 추가

--- src/main/java/com/project/tableforyou/auth/dto/LoginDto.java
+++ src/main/java/com/project/tableforyou/auth/dto/LoginDto.java
@@ -1,12 +1,15 @@
 package com.project.tableforyou.auth.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
 public class LoginDto {
 
+    @NotBlank(message = "아이디를 입력해주세요.")
     private String username;
+    @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
 }
 
