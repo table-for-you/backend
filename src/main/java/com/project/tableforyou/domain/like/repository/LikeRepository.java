c7a241c1ca4d9252bfc6e55b44cec0e72867a794
DaeYoung Park <pdy000726@naver.com>
2024-04-11 17:13:49 +0900

    feat - 가게 좋아요를 위한 엔티티 추가

--- src/main/java/com/project/tableforyou/domain/like/repository/LikeRepository.java
+++ src/main/java/com/project/tableforyou/domain/like/repository/LikeRepository.java
@@ -1,2 +1,15 @@
-package com.project.tableforyou.domain.like.repository;public interface LikeRepoository {
+package com.project.tableforyou.domain.like.repository;
+
+import com.project.tableforyou.domain.like.entity.Like;
+import com.project.tableforyou.domain.restaurant.entity.Restaurant;
+import com.project.tableforyou.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface LikeRepository extends JpaRepository<Like, Long> {
+
+    Optional<Like> findByUserAndRestaurant(User user, Restaurant restaurant);
+
+    boolean existsByUserAndRestaurant(User user, Restaurant restaurant);
 }
--- src/main/java/com/project/tableforyou/domain/like/service/LikeService.java
+++ src/main/java/com/project/tableforyou/domain/like/service/LikeService.java
@@ -1,2 +1,76 @@
-package com.project.tableforyou.domain.like.service;public class LikeService {
+package com.project.tableforyou.domain.like.service;
+
+import com.project.tableforyou.domain.like.entity.Like;
+import com.project.tableforyou.domain.like.repository.LikeRepository;
+import com.project.tableforyou.domain.restaurant.dto.RestaurantResponseDto;
+import com.project.tableforyou.domain.restaurant.entity.Restaurant;
+import com.project.tableforyou.domain.restaurant.repository.RestaurantRepository;
+import com.project.tableforyou.domain.user.entity.User;
+import com.project.tableforyou.domain.user.repository.UserRepository;
+import com.project.tableforyou.handler.exceptionHandler.CustomException;
+import com.project.tableforyou.handler.exceptionHandler.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class LikeService {
+
+    private final LikeRepository likeRepository;
+    private final UserRepository userRepository;
+    private final RestaurantRepository restaurantRepository;
+
+    /* 가게 좋아요 실행 메서드 */
+    @Transactional
+    public void likeRestaurant(String username, String restaurant_name) {
+
+        User user = userRepository.findByUsername(username).orElseThrow(() ->
+                new CustomException(ErrorCode.USER_NOT_FOUND));
+
+        Restaurant restaurant = restaurantRepository.findByName(restaurant_name).orElseThrow(() ->
+                new CustomException(ErrorCode.RESTAURANT_NOT_FOUND));
+
+        if(likeRepository.existsByUserAndRestaurant(user, restaurant)) {
+            throw new CustomException(ErrorCode.ALREADY_LIKE_RESTAURANT);
+        }
+
+        Like like = Like.builder()
+                .user(user)
+                .restaurant(restaurant)
+                .build();
+
+        likeRepository.save(like);
+    }
+
+    /* 가게 좋아요 취소 메서드 */
+    @Transactional
+    public void unLikeRestaurant(String username, String restaurant_name) {
+
+        User user = userRepository.findByUsername(username).orElseThrow(() ->
+                new CustomException(ErrorCode.USER_NOT_FOUND));
+
+        Restaurant restaurant = restaurantRepository.findByName(restaurant_name).orElseThrow(() ->
+                new CustomException(ErrorCode.RESTAURANT_NOT_FOUND));
+
+        Like like = likeRepository.findByUserAndRestaurant(user, restaurant).orElseThrow(() ->
+                new CustomException(ErrorCode.LIKE_NOT_FOUND));
+
+        likeRepository.delete(like);
+    }
+
+    @Transactional(readOnly = true)
+    public List<RestaurantResponseDto> getLikeRestaurants(String username) {
+
+        User user = userRepository.findByUsername(username).orElseThrow(() ->
+                new CustomException(ErrorCode.USER_NOT_FOUND));
+
+        return user.getLikes()
+                .stream()
+                .map(like -> new RestaurantResponseDto(like.getRestaurant()))
+                .collect(Collectors.toList());
+    }
 }
