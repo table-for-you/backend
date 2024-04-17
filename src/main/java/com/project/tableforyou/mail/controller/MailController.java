edf2be818d8abb3648a3d8aec8f82fb05d42b03f
DaeYoung Park <pdy000726@naver.com>
2024-04-18 02:12:18 +0900

    refactor - 메일 오류 처리

--- src/main/java/com/project/tableforyou/mail/controller/MailController.java
+++ src/main/java/com/project/tableforyou/mail/controller/MailController.java
@@ -24,22 +24,8 @@ public class MailController {
     @PostMapping("/emails/verification-request")
     public ResponseEntity<String> sendCodeToMail(@RequestParam("email") @Valid @Email String email) {
 
-        try {
-            if(email != null && !email.equals("")) {
-                boolean codeSent = codeService.sendCodeToMail(email);
-                if (codeSent) {
-                    return ResponseEntity.ok("인증메일 보내기 성공.");
-                } else {
-                    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("인증메일 보내기 실패. 1분 후 재전송.");
-                }
-            } else {
-                log.error("Invalid email address received from client: {}", email);
-                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("이메일을 제대로 입력하세요.");
-            }
-        } catch (Exception e) {
-            log.error("Failed to send verification email: {}", e.getMessage(), e);
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("인증메일 보내기 실패.");
-        }
+        codeService.sendCodeToMail(email);
+        return ResponseEntity.ok("인증메일 보내기 성공.");
     }
 
     /* 인증 번호 확인 */
@@ -47,13 +33,6 @@ public class MailController {
     public Object verifyCode(@RequestParam(value = "email") @Valid @Email String email,
                              @RequestParam("code") String code) {
 
-        if (codeService.verifiedCode(email, code)) {
-            if (userService.existsByEmail(email)) {
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("이미 가입된 이메일입니다.");
-            }
-            return true;
-        } else
-            return false;
-
+        return codeService.verifiedCode(email, code);
     }
 }
--- src/main/java/com/project/tableforyou/mail/service/CodeService.java
+++ src/main/java/com/project/tableforyou/mail/service/CodeService.java
@@ -1,8 +1,13 @@
 package com.project.tableforyou.mail.service;
 
+import com.project.tableforyou.domain.user.repository.UserRepository;
+import com.project.tableforyou.handler.exceptionHandler.error.ErrorCode;
+import com.project.tableforyou.handler.exceptionHandler.exception.CustomException;
 import com.project.tableforyou.mail.dto.CodeDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.security.NoSuchAlgorithmException;
@@ -17,33 +22,52 @@ import java.util.concurrent.ConcurrentHashMap;
 @Slf4j
 public class CodeService {
 
-
+    private final UserRepository userRepository;
     private final MailService mailService;
     private final Map<String, CodeDto> codeMap = new ConcurrentHashMap<>();  // 메일 인증번호 확인용. 멀티스레드 측면에서 안전성 이점.
     private static final int VERIFICATION_EXPIRATION_MINUTES = 3; // 이메일 인증 유효 시간 3분
     private static final int RESEND_VALIDITY_MINUTES = 1; // 이메일 재전송 유효 시간 1분
 
     /* 회원가입 인증번호 확인 메서드. */
-    public boolean sendCodeToMail(String key) {
-        CodeDto storedData = codeMap.get(key);
+    public void sendCodeToMail(String email) {
+
+        if(!checkEmail(email)) {
+            throw new CustomException(ErrorCode.INVALID_MAIL_ADDRESS);
+        }
+
+        if(!checkRetryEmail(email)) {
+            throw new CustomException(ErrorCode.ALREADY_MAIL_REQUEST);
+        }
+
+        String authCode = createCode();
+
+        mailService.sendMail(email, authCode);
+
+        codeMap.put(email, new CodeDto(authCode, LocalDateTime.now()));
+    }
+
+    /* 이메일 검증 */
+    private boolean checkEmail(String email) {
+        return email != null && !email.equals("");
+    }
+
+    /* 인증번호 재전송 시간 확인 */
+    private boolean checkRetryEmail(String email) {
+
+        CodeDto storedData = codeMap.get(email);
         if (storedData != null) {
             LocalDateTime currentTime = LocalDateTime.now();
             LocalDateTime expirationTime = storedData.getTimestamp().plusMinutes(RESEND_VALIDITY_MINUTES); // 이전에 보낸 메일로부터 1분이 지났는지 확인
 
             if (currentTime.isBefore(expirationTime)) {
                 // 이전에 보낸 메일로부터 1분이 지나지 않았으면 메일을 보낼 수 없음
-                log.warn("A recent code was sent less than a minute ago, so unable to send the code. {}", key);
                 return false;
             } else {
                 // 이전에 보낸 메일로부터 1분이 지났으면 해당 데이터 삭제
-                codeMap.remove(key);
+                codeMap.remove(email);
             }
         }
-        String authCode = createCode();
 
-        mailService.sendMail(key, authCode);
-
-        codeMap.put(key, new CodeDto(authCode, LocalDateTime.now()));
         return true;
     }
 
@@ -63,18 +87,24 @@ public class CodeService {
     }
 
     /* 인증번호 확인 메서드. */
-    public boolean verifiedCode(String key, String code) {
-        CodeDto storedCode = codeMap.get(key);
+    public boolean verifiedCode(String email, String code) {
+
+        if (userRepository.existsByEmail(email)) {
+            throw new CustomException(ErrorCode.ALREADY_EXISTS_MAIL);
+        }
+        System.out.println(userRepository.existsByEmail(email));
+
+        CodeDto storedCode = codeMap.get(email);
         if(storedCode != null && storedCode.getCode().equals(code)) {
             LocalDateTime expirationTime = storedCode.getTimestamp().plusMinutes(VERIFICATION_EXPIRATION_MINUTES);
 
             if (LocalDateTime.now().isBefore(expirationTime)) {     // 유효 시간이 지나지 않았다면
-                codeMap.remove(key);      // 인증코드가 맞다면 인증코드 삭제.
-                log.info("Authentication code verified successfully: {}", key);
+                codeMap.remove(email);      // 인증코드가 맞다면 인증코드 삭제.
+                log.info("Authentication code verified successfully: {}", email);
                 return true;
             } else {
-                codeMap.remove(key); // 유효 기간이 지났으면 해당 데이터 삭제
-                log.warn("Authentication code has expired: {}", key);
+                codeMap.remove(email); // 유효 기간이 지났으면 해당 데이터 삭제
+                log.warn("Authentication code has expired: {}", email);
             }
         }
         return false;
