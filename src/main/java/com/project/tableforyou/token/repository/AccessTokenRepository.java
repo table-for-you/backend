bc68382fa8fe4d8d459a74cd715b5176f07f4840
DaeYoung Park <pdy000726@naver.com>
2024-04-19 21:17:01 +0900

    fix - 사용하지 않는 필드 삭제

--- src/main/java/com/project/tableforyou/token/repository/AccessTokenRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.tableforyou.token.repository;
-
-import com.project.tableforyou.token.entity.AccessToken;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AccessTokenRepository extends CrudRepository<AccessToken, String> {
-}
