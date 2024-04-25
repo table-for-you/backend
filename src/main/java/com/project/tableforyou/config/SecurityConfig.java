c3cca11fa5e562f766425d9a386c9b5e3249a99f
DaeYoung Park <pdy000726@naver.com>
2024-04-26 00:21:30 +0900

    fix - 불필요한 Bean 삭제

--- src/main/java/com/project/tableforyou/config/SecurityConfig.java
+++ src/main/java/com/project/tableforyou/config/SecurityConfig.java
@@ -6,7 +6,6 @@ import com.project.tableforyou.handler.logoutHandler.SuccessLogoutHandler;
 import com.project.tableforyou.jwt.filter.JwtAuthenticationFilter;
 import com.project.tableforyou.jwt.filter.JwtExceptionFilter;
 import com.project.tableforyou.jwt.handler.OAuth2SuccessHandler;
-import com.project.tableforyou.security.auth.PrincipalDetailsService;
 import com.project.tableforyou.security.oauth.PrincipalOAuth2UserService;
 import com.project.tableforyou.token.service.TokenBlackListService;
 import com.project.tableforyou.utils.jwt.JwtUtil;
@@ -14,13 +13,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.filter.CorsFilter;
@@ -36,8 +33,6 @@ public class SecurityConfig {
     private final ObjectMapper objectMapper;
     private final OAuth2SuccessHandler oAuth2SuccessHandler;
     private final CustomLogoutHandler customLogoutHandler;
-    private final PrincipalDetailsService principalDetailsService;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final TokenBlackListService tokenBlackListService;
     private final SuccessLogoutHandler successLogoutHandler;
 
@@ -81,18 +76,6 @@ public class SecurityConfig {
 
 
         return http.build();
-    }
-
-    /* DaoAuthenticationProvider 구성 */
-    @Bean
-    public DaoAuthenticationProvider daoAuthenticationProvider() {
-
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(principalDetailsService);
-        authenticationProvider.setPasswordEncoder(bCryptPasswordEncoder);
-        authenticationProvider.setHideUserNotFoundExceptions(false);
-        // hideUserNotFoundExceptions를 false하며 UsernameNotFoundException 활성화
-        return  authenticationProvider;
     }
 
     //AuthenticationManager Bean 등록
