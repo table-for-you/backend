f8170f1d6307df31ef0c606e5debd55cac504892
DaeYoung Park <pdy000726@naver.com>
2024-05-13 00:31:26 +0900

    refactor - passwordEncoder 위치 변경

--- src/main/java/com/project/tableforyou/TableforyouApplication.java
+++ src/main/java/com/project/tableforyou/TableforyouApplication.java
@@ -2,17 +2,11 @@ package com.project.tableforyou;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableJpaAuditing // JPA Auditing 활성화
 public class TableforyouApplication {
-	@Bean
-	public BCryptPasswordEncoder bCryptPasswordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
 	public static void main(String[] args) {
 		SpringApplication.run(TableforyouApplication.class, args);
 	}
