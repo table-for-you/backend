7ccfd612f5073a30bc9b54d4204b55343b42c927
DaeYoung Park <pdy000726@naver.com>
2024-05-17 00:33:23 +0900

    fix - vaild 오류 문구 수정

--- src/main/java/com/project/tableforyou/domain/user/dto/UserUpdateDto.java
+++ src/main/java/com/project/tableforyou/domain/user/dto/UserUpdateDto.java
@@ -9,11 +9,11 @@ import lombok.Setter;
 public class UserUpdateDto {
 
     private String username;    // 본인 확인을 위한 username. 변경 불가하게 막아두기.
-    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-zA-Z0-9-_]{2,10}$")
-    @NotBlank(message = "닉네임은 특수문자를 제외한 2~10자리여야 합니다.")
+    @NotBlank(message = "닉네임은 필수 입력 값입니다.")
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-zA-Z0-9-_]{2,10}$", message = "닉네임은 특수문자를 제외한 2~10자리여야 합니다.")
     private String nickname;
-    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W).{8,16}")
-    @NotBlank(message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     private String password;
     private int age;
 }
---
+++ src/test/java/com/project/tableforyou/controller/AuthControllerTest.java
@@ -0,0 +1,2 @@
+package com.project.tableforyou.controller;public class AuthControllerTest {
+}
