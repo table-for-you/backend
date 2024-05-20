74272366d46360eca87d6c87c1f0315b887d6ab3
DaeYoung Park <pdy000726@naver.com>
2024-05-20 21:25:41 +0900

    refactor - EnableJpaAuditing 어노테이션 구분

--- src/main/java/com/project/tableforyou/TableforyouApplication.java
+++ src/main/java/com/project/tableforyou/TableforyouApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing // JPA Auditing 활성화
 public class TableforyouApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TableforyouApplication.class, args);
