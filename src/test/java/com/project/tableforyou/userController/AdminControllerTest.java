54db2ec2f1e10d4c532a75c4108847a9a6471022
DaeYoung Park <pdy000726@naver.com>
2024-05-30 18:40:04 +0900

    test - admin controller test 세팅

--- src/test/java/com/project/tableforyou/userController/AdminControllerTest.java
+++ src/test/java/com/project/tableforyou/userController/AdminControllerTest.java
@@ -1,2 +1,61 @@
-package com.project.tableforyou.userController;public class AdminControllerTest {
+package com.project.tableforyou.userController;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.tableforyou.domain.restaurant.service.AdminRestaurantService;
+import com.project.tableforyou.domain.user.controller.AdminController;
+import com.project.tableforyou.domain.user.entity.Role;
+import com.project.tableforyou.domain.user.entity.User;
+import com.project.tableforyou.domain.user.service.AdminService;
+import com.project.tableforyou.security.auth.PrincipalDetails;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+
+@WebMvcTest(AdminController.class)
+@AutoConfigureDataJpa
+public class AdminControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private AdminService adminService;
+
+    @MockBean
+    private AdminRestaurantService adminRestaurantService;
+
+    private User user;
+    private PrincipalDetails principalDetails;
+    @BeforeEach
+    public void setUp(WebApplicationContext webApplicationContext) {
+        this.mockMvc = MockMvcBuilders
+                .webAppContextSetup(webApplicationContext)
+                .apply(springSecurity())
+                .defaultRequest(get("/**").with(csrf()))
+                .defaultRequest(post("/**").with(csrf()))
+                .defaultRequest(put("/**").with(csrf()))
+                .defaultRequest(delete("/**").with(csrf()))
+                .build();
+
+        user = User.builder()
+                .username("admin")
+                .password("password")
+                .role(Role.ADMIN)
+                .build();
+
+        principalDetails = new PrincipalDetails(user);
+    }
 }
