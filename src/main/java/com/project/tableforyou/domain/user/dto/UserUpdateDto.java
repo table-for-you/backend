ad8aef9d4fce125a83d3d0a86735aecd500ff7fc
DaeYoung Park <pdy000726@naver.com>
2024-08-01 15:50:12 +0900

    docs - 문구 수정

--- src/main/java/com/project/tableforyou/domain/user/dto/UserUpdateDto.java
+++ src/main/java/com/project/tableforyou/domain/user/dto/UserUpdateDto.java
@@ -12,7 +12,7 @@ public class UserUpdateDto {
     @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-zA-Z0-9-_]{2,10}$", message = "닉네임은 특수문자를 제외한 2~10자리여야 합니다.")
     private String nickname;
     @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
-    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W).{8,16}", message = "비밀번호는 8~16자 영문자, 숫자, 특수문자를 사용하세요.")
     private String password;
     private int age;
 }
