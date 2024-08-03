5fb6b00391a280065ba6ebf13aec96b3b1ccc0d9
DaeYoung Park <pdy000726@naver.com>
2024-08-03 16:47:11 +0900

    fix: 로컬환경 테스트를 위한 cors 도메인 설정 임시 변경

--- src/main/java/com/project/tableforyou/config/CorsConfig.java
+++ src/main/java/com/project/tableforyou/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {       // Cors 정책을 벗어나기 위해 사용.
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.addAllowedOriginPattern("*");
         configuration.setAllowedMethods(List.of("GET", "POST", "OPTIONS", "PUT", "PATCH", "DELETE"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of(HttpHeaders.AUTHORIZATION, HttpHeaders.SET_COOKIE));
