f0da07f5a7b6ea3ad1608beb0966026a8304175d
DaeYoung Park <pdy000726@naver.com>
2024-09-17 20:58:04 +0900

    feat - Image 엔티티 추가

--- src/main/java/com/project/tableforyou/domain/image/entity/Image.java
+++ src/main/java/com/project/tableforyou/domain/image/entity/Image.java
@@ -1,2 +1,32 @@
-package com.project.tableforyou.domain.image.entity;public class Image {
+package com.project.tableforyou.domain.image.entity;
+
+import com.project.tableforyou.domain.restaurant.entity.Restaurant;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String url;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "restaurant_id")
+    private Restaurant restaurant;
 }
--- src/main/java/com/project/tableforyou/domain/image/repository/ImageRepository.java
+++ src/main/java/com/project/tableforyou/domain/image/repository/ImageRepository.java
@@ -1,7 +1,16 @@
-package com.project.tableforyou.domain.image.entity.repository;
+package com.project.tableforyou.domain.image.repository;
 
 import com.project.tableforyou.domain.image.entity.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
+
+    void deleteByUrl(String url);
+
+    @Query("SELECT i.url FROM Image i WHERE i.restaurant.id = :restaurantId")
+    List<String> findImageUrlsByRestaurantId(@Param("restaurantId") Long restaurantId);
 }
