47362eba256c6a592517976ccb061bd3e9832858
DaeYoung Park <pdy000726@naver.com>
2024-09-21 13:26:27 +0900

    feat - 단일 필드 업데이트 쿼리에 대한 DynamicUpdate 어노테이션 적용

--- src/main/java/com/project/tableforyou/domain/restaurant/entity/Restaurant.java
+++ src/main/java/com/project/tableforyou/domain/restaurant/entity/Restaurant.java
@@ -14,6 +14,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
 
 import java.util.List;
 
@@ -21,6 +22,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@DynamicUpdate
 @Entity
 public class Restaurant extends BaseTimeEntity {
     @Id
--- src/main/java/com/project/tableforyou/domain/user/entity/User.java
+++ src/main/java/com/project/tableforyou/domain/user/entity/User.java
@@ -20,6 +20,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -28,6 +29,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@DynamicUpdate
 @Entity
 public class User extends BaseTimeEntity {
     @Id
