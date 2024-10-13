3a18cac9b3b7102559c39bad6835d251240bdb79
DaeYoung0726 <pdy000726@naver.com>
2024-10-13 22:52:28 +0900

    feat: 레스토랑, 리뷰 권한 확인을 위한 aop 설정 추가

--- src/main/java/com/project/tableforyou/common/aop/VerifyAuthenticationAspect.java
+++ src/main/java/com/project/tableforyou/common/aop/VerifyAuthenticationAspect.java
@@ -1,16 +1,23 @@
 package com.project.tableforyou.common.aop;
 
+import com.project.tableforyou.common.aop.annotation.RestaurantId;
+import com.project.tableforyou.common.aop.annotation.ReviewId;
 import com.project.tableforyou.domain.restaurant.repository.RestaurantRepository;
 import com.project.tableforyou.common.handler.exceptionHandler.error.ErrorCode;
 import com.project.tableforyou.common.handler.exceptionHandler.exception.CustomException;
+import com.project.tableforyou.domain.review.repository.ReviewRepository;
 import com.project.tableforyou.security.auth.PrincipalDetails;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
 
 @Aspect
 @Component
@@ -19,22 +26,33 @@ import org.springframework.stereotype.Component;
 public class VerifyAuthenticationAspect {
 
     private final RestaurantRepository restaurantRepository;
+    private final ReviewRepository reviewRepository;
 
     @Around("@annotation(com.project.tableforyou.common.aop.annotation.VerifyAuthentication)")
     public Object verifyAuthentication(ProceedingJoinPoint joinPoint) throws Throwable {
 
-        Object[] args = joinPoint.getArgs();
-
         PrincipalDetails principalDetails = (PrincipalDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         String expectedUsername = principalDetails.getUsername();
 
-        for (Object arg : args) {
-            if (arg instanceof Long) {
-                Long restaurantId = (Long) arg;
-                restaurantVerifyAuthentication(expectedUsername, restaurantId);
-                break;      // 파라미터가 순서대로 들어가므로 MenuService의 update같이 타입이 같은게 있을 경우를 대비해 retaurantId만 받고 break한다.
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        Method method = methodSignature.getMethod();
+        Object[] args = joinPoint.getArgs();
+        Annotation[][] parameterAnnotations = method.getParameterAnnotations();
+
+        for (int i = 0; i < parameterAnnotations.length; i++) {
+            for (Annotation annotation : parameterAnnotations[i]) {
+                if (annotation instanceof RestaurantId) {
+                    Long restaurantId = (Long) args[i];
+                    restaurantVerifyAuthentication(expectedUsername, restaurantId);
+                    break;
+                } else if (annotation instanceof ReviewId) {
+                    Long reviewId = (Long) args[i];
+                    reviewVerifyAuthentication(expectedUsername, reviewId);
+                    break;
+                }
             }
         }
+
         log.info("Permission verified");
         return joinPoint.proceed();
     }
@@ -47,6 +65,16 @@ public class VerifyAuthenticationAspect {
             throw new CustomException(ErrorCode.RESTAURANT_NOT_FOUND);
         }
 
+        if (!expectedUsername.equals(actualUsername)) {
+            throw new CustomException(ErrorCode.UNAUTHORIZED);
+        }
+    }
+
+    private void reviewVerifyAuthentication(String expectedUsername, Long reviewId) {
+        String actualUsername = reviewRepository.findUsernameByReviewId(reviewId);
+        if (actualUsername == null) {
+            throw new CustomException(ErrorCode.REVIEW_NOT_FOUND);
+        }
         if (!expectedUsername.equals(actualUsername)) {
             throw new CustomException(ErrorCode.UNAUTHORIZED);
         }
