a2fadb662e84a8da5a1aa75d90a087e509e84b4a
DaeYoung0726 <pdy000726@naver.com>
2024-10-31 03:42:14 +0900

    refact: cors 허용 도메인 제한

--- src/main/java/com/project/tableforyou/config/CorsConfig.java
+++ src/main/java/com/project/tableforyou/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.project.tableforyou.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
@@ -12,10 +13,16 @@ import java.util.List;
 @Configuration
 public class CorsConfig {       // Cors 정책을 벗어나기 위해 사용.
 
+    @Value("${domain.local}")
+    private String localDomain;
+
+    @Value("${domain.server}")
+    private String serverDomain;
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOriginPattern("*");
+        configuration.setAllowedOrigins(List.of(localDomain, serverDomain));
         configuration.setAllowedMethods(List.of("GET", "POST", "OPTIONS", "PUT", "PATCH", "DELETE"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of(HttpHeaders.AUTHORIZATION, HttpHeaders.SET_COOKIE));
