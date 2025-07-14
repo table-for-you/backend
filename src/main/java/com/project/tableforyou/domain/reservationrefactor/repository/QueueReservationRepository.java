c589a71ceb66b409c9c1a47da90fc0ea92d9052f
DaeYoung0726 <pdy000726@naver.com>
2025-07-15 03:06:47 +0900

    feat: 번호표 예약(Queue) Repository 생성

---
+++ src/main/java/com/project/tableforyou/domain/reservationrefactor/repository/QueueReservationRepository.java
@@ -0,0 +1,19 @@
+package com.project.tableforyou.domain.reservationrefactor.repository;
+
+import com.project.tableforyou.domain.reservationrefactor.entity.QueueReservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+public interface QueueReservationRepository extends JpaRepository<QueueReservation, Long> {
+    int countByRestaurantIdAndDate(Long restaurantId, LocalDate date);
+
+    Optional<QueueReservation> findByUserIdAndRestaurantIdAndDateAndIsCanceledFalse(
+            Long userId, Long restaurantId, LocalDate date
+    );
+
+    int countByRestaurantIdAndDateAndIsEnteredTrue(Long restaurantId, LocalDate date);
+
+    int countByRestaurantIdAndDateAndIsCanceledTrue(Long restaurantId, LocalDate date);
+}
