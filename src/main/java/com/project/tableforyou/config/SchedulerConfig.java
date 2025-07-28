33f524bd95e336ea3d73e144ed78b56c566687f2
DaeYoung0726 <pdy000726@naver.com>
2025-07-29 02:44:39 +0900

    feat: 스케쥴링 빈 등록

---
+++ src/main/java/com/project/tableforyou/config/SchedulerConfig.java
@@ -0,0 +1,9 @@
+package com.project.tableforyou.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulerConfig {
+}
